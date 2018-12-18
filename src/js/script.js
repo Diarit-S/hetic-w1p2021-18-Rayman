a2673df3d7cedb45ea582a797fcf0356535ece85
ETombuyses <e.tombuyses@gmail.com>
2018-12-18 17:10:44 +0100

    smoke

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,12 @@ function game() {
     //initialisation des variables
     var character = document.getElementById('character');
     var finishline = document.getElementById('finishline');
+    var smoke = document.getElementById('smoke');
     var obstacles = document.querySelectorAll('.game__obstacle');
     var yellowjacket = document.getElementById('yellowjacket');
+    var scorejacket = 0;
+    document.querySelector('.game__score--jacket').innerText += " " +scorejacket + "/1";
+    
 
     //bouger character avec les keys directionnelles
     oxo.animation.moveElementWithArrowKeys(character, 30);
@@ -30,5 +34,8 @@ function game() {
     oxo.elements.onCollisionWithElementOnce(character, finishline, function() {
         //le niveau est fini
         console.log('niveau fini');
-    });
+    });   
+
+
+    
 };
