25f8e5884e6e2a31d884a52c29941e1280438500
Louis <louis.genestier@gmail.com>
2018-12-18 10:05:18 +0100

    add collision with finishline

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,24 @@ var direction;
 
 oxo.screens.loadScreen('game', function() {
     game();
-})
+});
 
 function game() {
     oxo.inputs.listenArrowKeys(function(key) {
         var character = document.getElementById('character');
         var ennemy = document.getElementById('obstacle');
+        var finishline = document.getElementById('finishline');
         direction = key;
         oxo.animation.move(character, direction, 10);
+        //Si collision entre character et obstacle
         oxo.elements.onCollisionWithElementOnce(character, ennemy, function() {
-            
+            //character est bloqué
+            console.log('collision avec obstacle');
+        });
+        //Si collision entre character et ligne d'arrivée
+        oxo.elements.onCollisionWithElementOnce(character, finishline, function() {
+            //le niveau est fini
+            console.log('niveau fini');
         });
     });
-}
+};
